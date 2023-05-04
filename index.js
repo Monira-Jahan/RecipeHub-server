9d59663a4c39c2574004f6616d680c2185b3d46a
Monira Jahan <monirajahan459@gmail.com>
2023-05-04 08:59:51 +0600

    Sever Section Added

---
+++ index.js
@@ -0,0 +1,16 @@
+const express=require('express');
+const app=express();
+//const cors=require('cors');
+const port= process.env.PORT || 5000;
+
+const chefs=require('./data/chefs.json')
+//app.use(cors());
+app.get('/',(req,res)=>{
+  res.send('Dragon is running');
+});
+app.get('/chefs',(req,res)=>{
+    res.send(chefs);
+  });
+app.listen(port,()=>{
+    console.log(`Dragon API is running on port:${port}`)
+})
