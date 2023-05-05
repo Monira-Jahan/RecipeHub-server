980402d5f22a8133fde01d3f6a926e5b2371eccf
Monira Jahan <monirajahan459@gmail.com>
2023-05-05 16:36:17 +0600

    ChefDetails Connection

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const cors=require('cors');
 const port= process.env.PORT || 5000;
 
 const chefs=require('./data/chefs.json');
-const recipes=require('./data/recipes.json')
+//const recipes=require('./data/recipes.json')
 app.use(cors());
 app.get('/',(req,res)=>{
   res.send('Dragon is running');
@@ -12,10 +12,10 @@ app.get('/',(req,res)=>{
 app.get('/chefs',(req,res)=>{
     res.send(chefs);
   });
-  app.get('/recipeDetails/:recipeId', (req, res) => {
+//   app.get('/recipedetails/:id', (req, res) => {
     
-    res.send(recipes);
-});
+//     res.send(chefs);
+// });
 app.listen(port,()=>{
     console.log(`Dragon API is running on port:${port}`)
 })
